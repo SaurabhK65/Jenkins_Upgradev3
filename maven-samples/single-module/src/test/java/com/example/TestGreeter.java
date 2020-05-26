13f509332f60a1ec0c9f9688ae88598659a6847d
Saurabh Kadam <kadamsaurabh650@gmail.com>
2020-05-26 16:34:54 +0530

    Change in File TestGreeter

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,4 +37,12 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+    @Test
+  public void greetShouldIncludeGreetingMessageCustom() {
+    String someone = "World, Thanks you making me Happy to learn Jenkins";
+  
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+    }
+
 }
